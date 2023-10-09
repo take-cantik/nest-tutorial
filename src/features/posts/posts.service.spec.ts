0f1b21d5a2084f8218bc8dfd2305cd0f58f84c4d
take-cantik <take.cantik17@gmail.com>
2023-10-09 23:56:58 +0900

    :adhesive_bandage: config path

--- src/features/posts/posts.service.spec.ts
+++ src/features/posts/posts.service.spec.ts
@@ -1,22 +1,34 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { PostService } from './posts.service';
-// import { PrismaService } from 'src/prisma.service';
-// import { PrismaClient } from '@prisma/client';
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaClient } from '@prisma/client';
+import { PostService } from './posts.service';
+import { PrismaService } from '~/prisma.service';
+import { generateUuid } from '~/utils/uuid';
 
-// describe('PostService', () => {
-//   let postService: PostService;
+describe('PostService', () => {
+  let postService: PostService;
 
-//   beforeEach(async () => {
-//     const app: TestingModule = await Test.createTestingModule({
-//       providers: [PrismaService],
-//     }).compile();
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      providers: [PostService, PrismaService],
+    }).compile();
 
-//     postService = app.get<PostService>(PostService);
-//   });
+    postService = app.get<PostService>(PostService);
+  });
 
-//   describe('findPostByPostId', () => {
-//     it('特定のIDのポストを取得できること', () => {
-//       const db = new PrismaClient();
-//     });
-//   });
-// });
+  describe('findPostByPostId', () => {
+    it('特定のIDのポストを取得できること', async () => {
+      const db = new PrismaClient();
+      const postId = generateUuid();
+      const expected = await db.post.create({
+        data: {
+          postId: postId,
+          title: 'post-test',
+          body: 'post-body',
+        },
+      });
+
+      const result = await postService.findPostByPostId({ postId });
+      expect(result).toStrictEqual(expected);
+    });
+  });
+});
