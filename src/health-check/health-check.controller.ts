a44b8031065ed77993faaf8c3bd925a3eb2d17bf
take-cantik <take.cantik17@gmail.com>
2023-10-09 22:08:43 +0900

    :sparkles: add health check controller

---
+++ src/health-check/health-check.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { HealthCheckService } from './health-check.service';
+
+@Controller('health-check')
+export class HealthCheckController {
+  constructor(private readonly healthCheckService: HealthCheckService) {}
+
+  @Get()
+  healthCheck(): string {
+    return this.healthCheckService.healthCheck();
+  }
+}
