a12ced31ab344a96130fb0d5c1741d56cce87b13
take-cantik <take.cantik17@gmail.com>
2023-10-09 22:44:20 +0900

    :sparkles: create post table

---
+++ prisma/migrations/20231009134400_create_post_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "postId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("postId")
+);
