fa7ca767f06d15e4bde0d37fac144daed6b1a09c
take-cantik <take.cantik17@gmail.com>
2023-10-11 12:20:14 +0900

    :sparkles: add findAuthUserByPublicId method for login

--- src/auth/repositories/auth-user.repository.ts
+++ src/auth/repositories/auth-user.repository.ts
@@ -1,8 +1,25 @@
 import { PrismaService } from '~/services/prisma.service';
 import { AuthUser } from '../domain/enitites/auth-user.entity';
+import { formatToStringFromDate } from '~/utils/day';
 
 export class AuthUserRepository {
   constructor(private prisma: PrismaService) {}
+
+  async findAuthUserByPublicId(publicId: string): Promise<AuthUser | null> {
+    const result = await this.prisma.user.findUnique({
+      where: { publicId },
+    });
+
+    if (!result) return null;
+
+    return new AuthUser(
+      result.userId,
+      result.publicId,
+      result.password,
+      formatToStringFromDate(result.createdAt),
+      formatToStringFromDate(result.updatedAt),
+    );
+  }
 
   async createAuthUser(authUser: AuthUser): Promise<void> {
     await this.prisma.user.create({
--- src/utils/day.ts
+++ src/utils/day.ts
@@ -7,14 +7,18 @@ dayjs.extend(timezone);
 
 dayjs.tz.setDefault('Asia/Tokyo');
 
-export const getDateFromUnixTime = (unixTime: number) => {
+export const formatToDateFromUnixTime = (unixTime: number) => {
   return dayjs(unixTime * 1000).format('YYYY-MM-DD');
 };
 
-export const getUnixTimeFromDate = (date: Date) => {
+export const formatToUnixTimeFromDate = (date: Date) => {
   return dayjs(date).unix();
 };
 
 export const now = (): string => {
   return dayjs().format('YYYY-MM-DDTHH:mm:ssZ[Z]');
+};
+
+export const formatToStringFromDate = (date: Date) => {
+  return dayjs(date).format('YYYY-MM-DDTHH:mm:ssZ[Z]');
 };
